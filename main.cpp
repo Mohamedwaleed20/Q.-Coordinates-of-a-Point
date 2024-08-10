72ef9762534a40f422c3cbebbb4070f3746b6f93
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-10 22:09:01 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+    double a, b;
+    std::cin >> a >> b;
+    if ((a == 0) && (b == 0))
+    {
+        std::cout << "Origem" << std::endl;
+    }
+    else if ((a == 0))
+    {
+        std::cout << "Eixo Y" << std::endl;
+    }
+    else if ((b == 0))
+    {
+        std::cout << "Eixo X" << std::endl;
+    }
+    else if ((a > 0) && (b > 0))
+    {
+        std::cout << "Q1" << std::endl;
+    }
+    else if ((a < 0) && (b > 0))
+    {
+        std::cout << "Q2" << std::endl;
+    }
+    else if ((a < 0) && (b < 0))
+    {
+        std::cout << "Q3" << std::endl;
+    }
+    else if ((a > 0) && (b < 0))
+    {
+        std::cout << "Q4" << std::endl;
+    }
+
+    return 0;
+}
